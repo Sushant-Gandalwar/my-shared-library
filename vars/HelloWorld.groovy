c6ec11aec3c0d79a36414de9ec1af2f694050560
Sushant Gandalwar <132839427+Sushan-Gandalwar@users.noreply.github.com>
2023-11-22 16:44:10 +0530

    Update HelloWorld.groovy

--- vars/HelloWorld.groovy
+++ vars/HelloWorld.groovy
@@ -1,3 +1,32 @@
-def sayHello(def name) {
-    echo "Hello, ${name}!"
+// MySharedLibrary/vars/DockerPipeline.groovy
+
+def call(String gitUrl, String gitBranch, String imageName) {
+    pipeline {
+        agent any
+
+        stages {
+            stage('Checkout') {
+                steps {
+                    checkout scmGit(branches: [[name: gitBranch]], extensions: [], userRemoteConfigs: [[url: gitUrl]])
+                    sh 'echo "Building the project"'
+                }
+            }
+
+            stage('Build Image') {
+                steps {
+                    script {
+                        sh "docker build -t ${imageName} ."
+                    }
+                }
+            }
+
+            stage('Access Image Locally') {
+                steps {
+                    script {
+                        sh "docker run -p 8085:3000 ${imageName}"
+                    }
+                }
+            }
+        }
+    }
 }
