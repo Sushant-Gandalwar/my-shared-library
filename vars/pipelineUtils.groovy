0fa0d92e2f4016b3bd73c4194a710aa18006fb34
Sushant Gandalwar <132839427+Sushan-Gandalwar@users.noreply.github.com>
2023-11-23 23:35:25 +0530

    Update pipelineUtils.groovy

--- vars/pipelineUtils.groovy
+++ vars/pipelineUtils.groovy
@@ -1,44 +1,37 @@
 // shared/utils/PipelineUtils.groovy
 package shared.utils
 
-class PipelineUtils {
-    static def createPipeline(Map pipelineConfig) {
-        echo "Creating pipeline with Git URL: ${pipelineConfig.scmUrl}"
+    class PipelineUtils {
+  static def createPipeline(Map pipelineConfig) {
+    echo "Creating pipeline with Git URL: ${pipelineConfig.scmUrl}"
 
         pipeline {
-            agent any
-            stages {
-                stage('Checkout') {
-                    steps {
-                        script {
-                            // Use the Git URL from the pipelineConfig
-                            checkout scm: [
-                                $class: 'GitSCM',
-                                branches: [[name: pipelineConfig.branch]],
-                                userRemoteConfigs: [[url: pipelineConfig.scmUrl]]
-                            ]
-                        }
-                    }
-                    stage('Build Image') {
-    steps {
-        script {
-            sh 'docker build -t jaydeep .'
-        }
-    }
-}
-
-stage('Access Image Locally') {
-    steps {
-        script {
-            sh 'docker run -p 8085:3000 jaydeep'
-        }
-    }
-}
-
-                }
-
-                // Add other stages as needed
+      agent any stages {
+        stage('Checkout') {
+          steps {
+            script {
+              // Use the Git URL from the pipelineConfig
+              checkout scm
+                  : [$class:'GitSCM',
+                                 branches:[[name:pipelineConfig.branch]],
+                        userRemoteConfigs:[[url:pipelineConfig.scmUrl]]]
             }
+          }
+          stage('Build Image') {
+            steps {
+              script { sh 'docker build -t jaydeep .' }
+            }
+          }
+
+          stage('Access Image Locally') {
+            steps {
+              script { sh 'docker run -p 8085:3000 jaydeep' }
+            }
+          }
         }
+
+        // Add other stages as needed
+      }
     }
+  }
 }
