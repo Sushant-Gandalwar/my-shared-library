b827521c11798f40ef25c21113832004a83336c2
Sushant Gandalwar <132839427+Sushan-Gandalwar@users.noreply.github.com>
2023-11-24 21:28:19 +0530

    Update pipelineUtils.groovy

--- vars/pipelineUtils.groovy
+++ vars/pipelineUtils.groovy
@@ -1,39 +1,28 @@
 pipeline {
     agent any
 
-    environment {
-        // Define environment variables if needed
-    }
-
     stages {
-        stage('INITIALIZE') {
+        stage('Checkout') {
+            steps {
+                checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: scmUrl]])
+                sh 'echo "Building the project"'
+            }
+        }
+
+        stage('build image') {
             steps {
                 script {
-                    echo "Initializing environment for webstore delivery pipeline"
-                    echo 'Start Initializing!'
-                    def valuesYaml = loadValuesYaml()
-                    git branch: pipelineParams.branch, credentialsId: pipelineParams.bitbucketCredentialsId, url: pipelineParams.scmUrl
-
-                    if (env.IMAGE_TAG == 'default' && pipelineParams.branch == 'master') {
-                        // Additional logic for handling conditions
-                    }
+                    sh "docker build -t ${dockerImage} ."
                 }
             }
         }
 
-        // Add more stages as needed
-    }
-
-    post {
-        always {
-            // Cleanup or other actions that should be performed regardless of the pipeline result
+        stage('access image locally') {
+            steps {
+                script {
+                    sh "docker run -p 8085:80 ${dockerImage}"
+                }
+            }
         }
     }
-}
-
-def loadValuesYaml() {
-    // Implement your logic to load values from a YAML file
-    // For example, you can use a library like SnakeYAML
-    // Here, a dummy map is returned for illustration purposes
-    return [key1: 'value1', key2: 'value2']
 }
