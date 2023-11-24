c536043e9f5168108a126f5197d1961b2edfff4c
Sushant Gandalwar <132839427+Sushan-Gandalwar@users.noreply.github.com>
2023-11-24 13:57:06 +0530

    Update pipelineUtils.groovy

--- vars/pipelineUtils.groovy
+++ vars/pipelineUtils.groovy
@@ -1,4 +1,4 @@
-// shared/utils/PipelineUtils.groovy
+
 package shared.utils class PipelineUtils {
     static def createPipeline(Map pipelineConfig) {
         echo "Creating pipeline with Git URL: ${pipelineConfig.scmUrl}"
@@ -32,7 +32,7 @@ package shared.utils class PipelineUtils {
                         }
                     }
 
-                    // Add other stages as needed
+                    
                 }
             }
         }
