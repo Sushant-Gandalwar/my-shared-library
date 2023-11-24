f8cf412085226bf6e35c8ad2efa49c69479a45ac
Sushant Gandalwar <132839427+Sushan-Gandalwar@users.noreply.github.com>
2023-11-24 21:57:34 +0530

    Update pipelineUtils.groovy

--- vars/pipelineUtils.groovy
+++ vars/pipelineUtils.groovy
@@ -1,28 +1,40 @@
-pipeline {
-    agent any
+// common.groovy
 
-    stages {
-        stage('Checkout') {
-            steps {
-                checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: scmUrl]])
-                sh 'echo "Building the project"'
-            }
-        }
-
-        stage('build image') {
-            steps {
-                script {
-                    sh "docker build -t ${dockerImage} ."
-                }
-            }
-        }
-
-        stage('access image locally') {
-            steps {
-                script {
-                    sh "docker run -p 8085:80 ${dockerImage}"
-                }
+def checkoutAndBuildProject(scmUrl, branch, appName, buildCommand) {
+    stage('Checkout') {
+        steps {
+            script {
+                checkout scmGit(scmUrl, branch)
+                sh buildCommand
             }
         }
     }
+}
+
+def buildDockerImage(imageName, dockerfile) {
+    stage('Build Docker Image') {
+        steps {
+            script {
+                sh "docker build -t $imageName -f $dockerfile ."
+            }
+        }
+    }
+}
+
+def runDockerContainer(portMapping, imageName) {
+    stage('Run Docker Container') {
+        steps {
+            script {
+                sh "docker run -p $portMapping $imageName"
+            }
+        }
+    }
+}
+
+def scmGit(scmUrl, branch) {
+    return [
+        $class: 'GitSCM',
+        branches: [[name: branch]],
+        userRemoteConfigs: [[url: scmUrl]]
+    ]
 }
