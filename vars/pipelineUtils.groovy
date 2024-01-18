c7867848be29c4ad2caa51df54cace68b8f44467
Sushant-Gandalwar <153522559+Sushant-Gandalwar@users.noreply.github.com>
2024-01-18 17:36:12 +0530

    Update pipelineUtils.groovy

--- vars/pipelineUtils.groovy
+++ vars/pipelineUtils.groovy
@@ -64,30 +64,39 @@ def call(Map pipelineParams) {
                 }
             }
             stage('PUBLISH IMAGE') {
-               when {
-                     expression { params.Build_Type == 'BUILD&DEPLOY&Publish_to_snapshot' }
-               }                        
-                steps {
-                    script {
-                        echo "Building docker image and publishing to GCR"
-                    }
-                    sh "sbt publish"
-                    sh "sbt docker:publishLocal"
-                    withDockerRegistry([credentialsId: "gcr:${env.CREDENTIALS_ID}", url: "https://hub.docker.com/orgs"]) {
-                      sh "cd ${env.DOCKERDIRECTORY} && docker build -t '${env.IMAGE}:${env.IMAGETAG}' -f Dockerfile ."
-                      sh """
-                         docker push '${env.IMAGE}:${env.IMAGETAG}'
-                         docker rmi '${env.IMAGE}:${env.IMAGETAG}'
-                         
-                         """
-                    }
+    when {
+        expression { params.Build_Type == 'BUILD&DEPLOY&Publish_to_snapshot' }
+    }
+    steps {
+        script {
+            echo "Building docker image and publishing to GCR"
+        }
 
+        // Ensure that sbt and Docker are installed
+        sh 'sbt clean compile'
 
-                    script {
-                        echo "Published Docker image ${env.IMAGE} to GCR"
-                    }
-                }
+        // Publish the Docker image locally using sbt
+        sh 'sbt docker:publishLocal'
+
+        // Build and push the Docker image to GCR
+        withDockerRegistry([credentialsId: "gcr:${env.CREDENTIALS_ID}", url: "https://gcr.io"]) {
+            sh "cd ${env.DOCKERDIRECTORY} && docker build -t '${env.IMAGE}:${env.IMAGETAG}' -f Dockerfile ."
+            sh "docker push '${env.IMAGE}:${env.IMAGETAG}'"
+            sh "docker rmi '${env.IMAGE}:${env.IMAGETAG}'"
+        }
+
+        script {
+            echo "Published Docker image ${env.IMAGE}:${env.IMAGETAG} to GCR"
+        }
+    }
+    post {
+        failure {
+            script {
+                echo "Failed to build and publish Docker image."
             }
+        }
+    }
+}
 
         }
     }
