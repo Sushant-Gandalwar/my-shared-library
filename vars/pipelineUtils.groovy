9040fccc0b6551faba1b57521aa5b06bdb1fd58e
Sushant-Gandalwar <153522559+Sushant-Gandalwar@users.noreply.github.com>
2024-01-18 13:48:26 +0530

    Update pipelineUtils.groovy

--- vars/pipelineUtils.groovy
+++ vars/pipelineUtils.groovy
@@ -1,28 +1,25 @@
 def call(Map pipelineParams) {
-   pipeline{
-    agent any 
-       parameters {
-            choice(name: 'Build_Type', choices: "BUILD&DEPLOY&Publish_to_snapshot\nDEPLOY_ONLY\nPublish_to_Release", description: 'Select the Build type' )
-           string(name: 'Parameter', defaultValue: 'default', description: 'Pass the Docker image id if choosed DEPLOY_ONLY OR pass the sbt release command if choosed Publish_to_Release', )
-        }        
+    pipeline {
+        agent any
+        parameters {
+            choice(name: 'Build_Type', choices: "BUILD&DEPLOY&Publish_to_snapshot\nDEPLOY_ONLY\nPublish_to_Release", description: 'Select the Build type')
+            string(name: 'Parameter', defaultValue: 'default', description: 'Pass the Docker image id if choosed DEPLOY_ONLY OR pass the sbt release command if choosed Publish_to_Release',)
+        }
 
         environment {
             scmUrl = "${pipelineParams.scmUrl}"
             APP_Name = "${pipelineParams.appName}"
-            }
-
+        }
 
         stages {
             stage('INITIALIZE') {
                 steps {
-                   script{
-                     log.info("Initializing environment for webstore delivery pipeline")
-                    echo "git Initialize env.scmUrl"
-                    
-                   }
+                    script {
+                        log.info("Initializing environment for webstore delivery pipeline")
+                        echo "Git URL: ${env.scmUrl}"
+                    }
                 }
             }
         }
-   }
+    }
 }
-
