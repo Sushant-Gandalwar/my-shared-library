c423e9be92cd35882bbd4a27f4ae09f4062d2332
Sushant-Gandalwar <153522559+Sushant-Gandalwar@users.noreply.github.com>
2024-01-18 15:04:49 +0530

    Update pipelineUtils.groovy

--- vars/pipelineUtils.groovy
+++ vars/pipelineUtils.groovy
@@ -36,6 +36,31 @@ def call(Map pipelineParams) {
                     }
                 }
             }
+            stage('PUBLISH IMAGE') {
+               when {
+                     expression { params.Build_Type == 'BUILD&DEPLOY&Publish_to_snapshot' }
+               }                        
+                steps {
+                    script {
+                        echo "Building docker image and publishing to GCR"
+                    }
+                    sh "sbt publish"
+                    sh "sbt docker:publishLocal"
+                    withDockerRegistry([credentialsId: "gcr:${env.CREDENTIALS_ID}", url: "https://gcr.io"]) {
+                      sh "cd ${env.DOCKERDIRECTORY} && docker build -t '${env.IMAGE}:${env.IMAGETAG}' -f Dockerfile ."
+                      sh """
+                         docker push '${env.IMAGE}:${env.IMAGETAG}'
+                         docker rmi '${env.IMAGE}:${env.IMAGETAG}'
+                         
+                         """
+                    }
+
+
+                    script {
+                        echo "Published Docker image ${env.IMAGE} to GCR"
+                    }
+                }
+            }
         
         }
     }
