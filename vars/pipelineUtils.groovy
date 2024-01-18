b6fd49e76ef6cc620614fcbc57d0362f4aca6847
Sushant-Gandalwar <153522559+Sushant-Gandalwar@users.noreply.github.com>
2024-01-18 12:49:43 +0530

    Update pipelineUtils.groovy

--- vars/pipelineUtils.groovy
+++ vars/pipelineUtils.groovy
@@ -6,35 +6,16 @@ def call(Map pipelineParams) {
            string(name: 'Parameter', defaultValue: 'default', description: 'Pass the Docker image id if choosed DEPLOY_ONLY OR pass the sbt release command if choosed Publish_to_Release', )
         }        
 
-        environment {
-            APP_NAME = "${pipelineParams.appName}"
-            IMAGE_TAG = "${params.Parameter}"
-            COMMAND = "${params.Parameter}"
-	        BRANCH =  "${pipelineParams.branch}"
-            }
-
+        
 
         stages {
             stage('INITIALIZE') {
                 steps {
-                    script {
-                        log.info("Initializing environment for webstore delivery pipeline")
-                        echo 'Start Initializing!'
-                        git branch: pipelineParams.branch,url: pipelineParams.scmUrl                  
-                          if (env.IMAGE_TAG == 'default' && pipelineParams.branch == 'main') {
-                          env.PACKAGE_VERSION = sh(
-                               script: """
-                                 set +x
-                                 cat version.sbt | cut -d '=' -f2 | tr -d '\"'
-                               """,
-                               returnStdout: true
-                               ).trim()
-
-                          env.IMAGETAG = 'v' + env.PACKAGE_VERSION + '-' + env.BUILD_NUMBER
-                        }                      
+                       script {
+                        echo "Git URL: ${gitUrl}"
+                        // Additional initialization steps using the Git URL
                     }
-                }
-            }
+		}
         }
    }
 }
