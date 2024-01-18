d8d70282a7d806c28607b68e22db53995794480b
Sushant-Gandalwar <153522559+Sushant-Gandalwar@users.noreply.github.com>
2024-01-18 15:03:26 +0530

    Update pipelineUtils.groovy

--- vars/pipelineUtils.groovy
+++ vars/pipelineUtils.groovy
@@ -17,56 +17,26 @@ def call(Map pipelineParams) {
         environment {
             scmUrl = "${pipelineParams.scmUrl}"
             APP_Name = "${pipelineParams.appName}"
-            DOCKERDIRECTORY = "${pipelineParams.dockerDirectory}"
-            IMAGE = "${pipelineParams.dockerImage}"
-            CREDENTIALS_ID = "${pipelineParams.dockerCredentialsId}"
-            IMAGE_TAG = "${params.Parameter}"
         }
 
         stages {
             stage('INITIALIZE') {
                 steps {
                     script {
+                        echo "Initializing environment for webstore delivery pipeline"
+                        echo "Git URL: ${env.scmUrl}"
                         
-                        log.info("Initializing environment for webstore delivery pipeline")
-                        log.info("Git URL: ${env.scmUrl}")
                     }
                 }
                 post {
                     failure {
                         script {
-                            
                             log.error("Initialization code has an error for ${APP_Name}")
                         }
                     }
                 }
             }
-
-            stage('PUBLISH IMAGE') {
-                when {
-                    expression { params.Build_Type == 'BUILD&DEPLOY&Publish_to_snapshot' }
-                }
-                steps {
-                    script {
-                        
-                        log.info("Building docker image and publishing to GCR")
-                    }
-                    sh "sbt publish"
-                    sh "sbt docker:publishLocal"
-                    withDockerRegistry([credentialsId: "gcr:${env.CREDENTIALS_ID}", url: "https://hub.docker.com/orgs"]) {
-                        sh "cd ${env.DOCKERDIRECTORY} && docker build -t '${env.IMAGE}:${env.IMAGETAG}' -f Dockerfile ."
-                        sh """
-                           docker push '${env.IMAGE}:${env.IMAGETAG}'
-                           docker rmi '${env.IMAGE}:${env.IMAGETAG}'
-                        """
-                    }
-
-                    script {
-                        def log =  new LogWrapper(currentBuild.rawBuild.logger)  // Initialize log
-                        log.info("Published Docker image ${env.IMAGE} to GCR")
-                    }
-                }
-            }
+        
         }
     }
 }
