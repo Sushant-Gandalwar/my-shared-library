9153ece3601d50c38d9d795632ceea7d90478353
Sushant-Gandalwar <153522559+Sushant-Gandalwar@users.noreply.github.com>
2024-01-18 17:25:40 +0530

    Update pipelineUtils.groovy

--- vars/pipelineUtils.groovy
+++ vars/pipelineUtils.groovy
@@ -17,6 +17,11 @@ def call(Map pipelineParams) {
         environment {
             scmUrl = "${pipelineParams.scmUrl}"
             APP_Name = "${pipelineParams.appName}"
+            DOCKERDIRECTORY = "${pipelineParams.dockerDirectory}"
+            IMAGE = "${pipelineParams.dockerImage}"
+            CREDENTIALS_ID = "${pipelineParams.dockerCredentialsId}"
+            // DB_CREDS_ID = "${pipelineParams.databaseCredentialsId}"
+            IMAGE_TAG = "${params.Parameter}"
         }
 
         stages {
@@ -42,13 +47,13 @@ def call(Map pipelineParams) {
             }            
                 steps {
                     script {
-                       echo "Running Reload, clean and compile"
+                        echo "Running Reload, clean and compile"
                     }
 		     sh ''' 
 		          java -version 
 		       	'''
                     sh "sbt reload && sbt clean && sbt compile"
-		   // sh "sbt dependencyTree"
+		  
                 }
                 post {
                   failure {
@@ -58,6 +63,32 @@ def call(Map pipelineParams) {
                   }
                 }
             }
+            stage('PUBLISH IMAGE') {
+               when {
+                     expression { params.Build_Type == 'BUILD&DEPLOY&Publish_to_snapshot' }
+               }                        
+                steps {
+                    script {
+                        echo "Building docker image and publishing to GCR"
+                    }
+                    sh "sbt publish"
+                    sh "sbt docker:publishLocal"
+                    withDockerRegistry([credentialsId: "gcr:${env.CREDENTIALS_ID}", url: "https://hub.docker.com/orgs"]) {
+                      sh "cd ${env.DOCKERDIRECTORY} && docker build -t '${env.IMAGE}:${env.IMAGETAG}' -f Dockerfile ."
+                      sh """
+                         docker push '${env.IMAGE}:${env.IMAGETAG}'
+                         docker rmi '${env.IMAGE}:${env.IMAGETAG}'
+                         
+                         """
+                    }
+
+
+                    script {
+                        echo "Published Docker image ${env.IMAGE} to GCR"
+                    }
+                }
+            }
+
         }
     }
 }
