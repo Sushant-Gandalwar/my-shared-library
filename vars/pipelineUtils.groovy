43d3ec34de449c97c40399b83004fc34e418866d
Sushant-Gandalwar <153522559+Sushant-Gandalwar@users.noreply.github.com>
2024-01-18 13:28:24 +0530

    Update pipelineUtils.groovy

--- vars/pipelineUtils.groovy
+++ vars/pipelineUtils.groovy
@@ -1,19 +1,26 @@
-// vars/MySharedLibrary.groovy
+def call(Map pipelineParams) {
+   pipeline{
+    agent any 
+       parameters {
+            choice(name: 'Build_Type', choices: "BUILD&DEPLOY&Publish_to_snapshot\nDEPLOY_ONLY\nPublish_to_Release", description: 'Select the Build type' )
+           string(name: 'Parameter', defaultValue: 'default', description: 'Pass the Docker image id if choosed DEPLOY_ONLY OR pass the sbt release command if choosed Publish_to_Release', )
+        }        
 
-def call(String gitUrl) {
-    pipeline {
-        agent any
-        
+        environment {
+            scmUrl = "${pipelineParams.scmUrl}"
+            }
+
+
         stages {
-            stage('Initialize') {
+            stage('INITIALIZE') {
                 steps {
-                    script {
-                        echo "Git URL: ${gitUrl}"
-                        // Additional initialization steps using the Git URL
-                    }
+                   script{
+                    echo "git Initialize env.scmUrl"
+                    
+                   }
                 }
             }
-            // Other stages would follow...
         }
-    }
+   }
 }
+
