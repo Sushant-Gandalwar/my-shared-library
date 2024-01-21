3217ac3b3e1270cabaf58f47ab989dec8785f9f8
Sushant-Gandalwar <153522559+Sushant-Gandalwar@users.noreply.github.com>
2024-01-21 15:06:54 +0530

    Update index.groovy

--- vars/index.groovy
+++ vars/index.groovy
@@ -1,1 +1,50 @@
+def call(Map pipelineParams) {
+    pipeline {
+        agent any
+        parameters {
+            choice(
+                name: 'Build_Type',
+                choices: 'BUILD&DEPLOY&Publish_to_snapshot\nDEPLOY_ONLY\nPublish_to_Release',
+                description: 'Select the Build type'
+            )
+            string(
+                name: 'Parameter',
+                defaultValue: 'default',
+                description: 'Pass the Docker image id if choosing DEPLOY_ONLY OR pass the sbt release command if choosing Publish_to_Release'
+            )
+        }
+
+        environment {
+            scmUrl = "${pipelineParams.scmUrl}"
+            APP_Name = "${pipelineParams.appName}"
+            DOCKERDIRECTORY = "${pipelineParams.dockerDirectory}"
+            IMAGE = "${pipelineParams.dockerImage}"
+            CREDENTIALS_ID = "${pipelineParams.dockerCredentialsId}"
+            // CREDENTIALS_PASS = "${pipelineParams.dockerCredentialsPass}"
+            // DB_CREDS_ID = "${pipelineParams.databaseCredentialsId}"
+            IMAGE_TAG = "${params.Parameter}"
+        }
+
+        stages {
+            stage('INITIALIZE') {
+                steps {
+                    script {
+                        echo "Initializing environment for webstore delivery pipeline"
+                        echo "Git URL: ${env.scmUrl}"
+                        
+                    }
+                }
+                post {
+                    failure {
+                        script {
+                            log.error("Initialization code has an error for ${APP_Name}")
+                        }
+                    }
+                }
+            }
+            
+          
+        }
+    }
+}
 
