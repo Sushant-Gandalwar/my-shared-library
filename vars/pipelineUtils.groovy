f4a632dbc170bf955f6e2cbd20675b0f761e9a87
Sushant-Gandalwar <153522559+Sushant-Gandalwar@users.noreply.github.com>
2024-01-21 12:28:57 +0530

    Update pipelineUtils.groovy

--- vars/pipelineUtils.groovy
+++ vars/pipelineUtils.groovy
@@ -20,7 +20,7 @@ def call(Map pipelineParams) {
             DOCKERDIRECTORY = "${pipelineParams.dockerDirectory}"
             IMAGE = "${pipelineParams.dockerImage}"
             CREDENTIALS_ID = "${pipelineParams.dockerCredentialsId}"
-            CREDENTIALS_PASS = "${pipelineParams.dockerCredentialsPass}"
+            // CREDENTIALS_PASS = "${pipelineParams.dockerCredentialsPass}"
             // DB_CREDS_ID = "${pipelineParams.databaseCredentialsId}"
             IMAGE_TAG = "${params.Parameter}"
         }
@@ -64,50 +64,32 @@ def call(Map pipelineParams) {
                   }
                 }
             }
-            stage('BUILD IMAGE') {
-    when {
-        expression { params.Build_Type == 'BUILD&DEPLOY&Publish_to_snapshot' }
-    }
-    steps {
-        script {
-            echo "Building Docker image"
-        }
+            stage('PUBLISH IMAGE') {
+               when {
+                     expression { params.Build_Type == 'BUILD&DEPLOY&Publish_to_snapshot' }
+               }                        
+                steps {
+                    script {
+                        log.info("Building docker image and publishing to GCR")
+                    }
+                    sh "sbt publish"
+                    sh "sbt docker:publishLocal"
+                    withDockerRegistry([credentialsId: "gcr:${env.CREDENTIALS_ID}", url: "https://index.docker.io/v1/"]) {
+                      sh "cd ${env.DOCKERDIRECTORY} && docker build -t '${env.IMAGE}:${env.IMAGETAG}' -f Dockerfile ."
+                      sh """
+                         docker push '${env.IMAGE}:${env.IMAGETAG}'
+                         docker rmi '${env.IMAGE}:${env.IMAGETAG}'
+                         
+                         """
+                    }
 
-        // Ensure that sbt is installed
-        sh 'sbt clean compile'
 
-        // Publish the Docker image locally using sbt
-        sh 'sbt docker:publishLocal'
-
-        withDockerRegistry([credentialsId: "dockerhub-credentials", url: "https://index.docker.io/v1/"]) {
-    // Change to the Docker directory
-    dir("${env.DOCKERDIRECTORY}") {
-        // Build the Docker image
-        sh "docker build -t '${env.IMAGE}:${env.IMAGETAG}' -f Dockerfile ."
-
-        // Log in to Docker Hub (using credentials)
-        sh "docker login -u ${env.credentialsId} -p ${env.credentialsPass}"
-
-        // Push the Docker image to Docker Hub
-        sh "docker push '${env.IMAGE}:${env.IMAGETAG}'"
-
-        // Log out from Docker Hub
-        sh "docker logout"
-
-        // Remove the local Docker image (optional)
-        sh "docker rmi '${env.IMAGE}:${env.IMAGETAG}'"
-    }
-}
-
-    }
-    post {
-        failure {
-            script {
-                echo "Failed to build Docker image."
+                    script {
+                        log.info("Published Docker image ${env.IMAGE} to GCR")
+                    }
+                }
             }
-        }
-    }
-}
+
 
         }
     }
