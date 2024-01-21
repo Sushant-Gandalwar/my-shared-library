10bacb28e6d5345b606b94c0cd31ced907a058ed
Sushant-Gandalwar <153522559+Sushant-Gandalwar@users.noreply.github.com>
2024-01-21 15:08:31 +0530

    Update index.groovy

--- vars/index.groovy
+++ vars/index.groovy
@@ -1,28 +1,11 @@
 def call(Map pipelineParams) {
     pipeline {
         agent any
-        parameters {
-            choice(
-                name: 'Build_Type',
-                choices: 'BUILD&DEPLOY&Publish_to_snapshot\nDEPLOY_ONLY\nPublish_to_Release',
-                description: 'Select the Build type'
-            )
-            string(
-                name: 'Parameter',
-                defaultValue: 'default',
-                description: 'Pass the Docker image id if choosing DEPLOY_ONLY OR pass the sbt release command if choosing Publish_to_Release'
-            )
-        }
+        
 
         environment {
             scmUrl = "${pipelineParams.scmUrl}"
-            APP_Name = "${pipelineParams.appName}"
-            DOCKERDIRECTORY = "${pipelineParams.dockerDirectory}"
-            IMAGE = "${pipelineParams.dockerImage}"
-            CREDENTIALS_ID = "${pipelineParams.dockerCredentialsId}"
-            // CREDENTIALS_PASS = "${pipelineParams.dockerCredentialsPass}"
-            // DB_CREDS_ID = "${pipelineParams.databaseCredentialsId}"
-            IMAGE_TAG = "${params.Parameter}"
+            
         }
 
         stages {
@@ -47,4 +30,3 @@ def call(Map pipelineParams) {
         }
     }
 }
-
