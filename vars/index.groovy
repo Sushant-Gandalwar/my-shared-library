11848853bc939d87d5bacf7475f2fc7985f4c4c8
Sushant-Gandalwar <153522559+Sushant-Gandalwar@users.noreply.github.com>
2024-01-21 21:31:12 +0530

    Update index.groovy

--- vars/index.groovy
+++ vars/index.groovy
@@ -9,6 +9,8 @@ def call(Map pipelineParams) {
             IMAGE_TAG = "${params.Parameter}"
             IMAGE = "${pipelineParams.dockerImage}"
             CREDENTIALS_ID = "${pipelineParams.dockerCredentialsId}"
+             CREDENTIALS_PASS = "${pipelineParams.dockerCredentialsPass}"
+             DOCKER_HUB_USERNAME = "${pipelineParams.dockerUsername}"
         }
 
         stages {
@@ -34,23 +36,17 @@ def call(Map pipelineParams) {
                         dir(env.DOCKERDIRECTORY) {
                             sh "docker build -t ${env.APP_Name}:${env.IMAGE_TAG} -f Dockerfile ."
                         }
-                        //  withCredentials([string(credentialsId: env.DOCKER_HUB_CREDENTIALS_ID, variable: 'DOCKER_HUB_CREDENTIALS')]) {
-                        //     sh "echo ${DOCKER_HUB_CREDENTIALS} | docker login -u sushantgandalwar --password-stdin"
-                        // }
-
-                          withDockerRegistry([credentialsId: "gcr:${env.CREDENTIALS_ID}", url: "https://hub.docker.com/"]) {
-                      sh "cd ${env.DOCKERDIRECTORY} && docker build -t '${env.IMAGE}:${env.IMAGETAG}' -f Dockerfile ."
-                          }
+                        
 
                        
                         // Login to Docker Hub
-                        // sh "docker login -u ${env.DOCKER_HUB_USERNAME} -p ${env.DOCKER_HUB_PASSWORD}"
+                        sh "docker login -u ${env.CREDENTIALS_ID} -p ${env.CREDENTIALS_PASS}"
 
                         // Tag the Docker image
-                        // sh "docker tag ${env.IMAGE}:${env.IMAGE_TAG} ${env.DOCKER_HUB_USERNAME}/${env.APP_Name}:latest"
+                        sh "docker tag ${env.APP_Name}:${env.IMAGE_TAG} ${env.DOCKER_HUB_USERNAME}/${env.APP_Name}:latest"
 
                         // Push the Docker image to Docker Hub
-                        // sh "docker push ${env.DOCKER_HUB_USERNAME}/${env.APP_Name}:latest"
+                        sh "docker push ${env.DOCKER_HUB_USERNAME}/${env.APP_Name}:latest"
                     }
                 }
             }
