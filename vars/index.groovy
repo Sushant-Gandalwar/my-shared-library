b546ff11e9cd4814773456d6a9cc05fa54b924f9
Sushant-Gandalwar <153522559+Sushant-Gandalwar@users.noreply.github.com>
2024-01-22 16:40:46 +0530

    Update index.groovy

--- vars/index.groovy
+++ vars/index.groovy
@@ -35,10 +35,11 @@ def call(Map pipelineParams) {
                             // Build the Docker image
                             sh "docker build -t ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} -f Dockerfile ."
 
-                            // Login to Docker Hub and push the image
-                            sh "docker login -u ${DOCKER_HUB_USERNAME} -p ${DOCKER_HUB_PASSWORD}"  // Ensure DOCKER_HUB_PASSWORD is available in Jenkins credentials
-                            // sh "docker push ${DOCKER_HUB_USERNAME}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}"
-                            // sh "docker logout"
+                            // Push the Docker image to Docker Hub
+                            withDockerRegistry([credentialsId: "${DOCKERHUB_CREDENTIALS}", url: "https://index.docker.io/v1/"]) {
+                                sh "docker login -u _json_key -p \$(cat \${DOCKERHUB_CREDENTIALS}_json_key.json) https://index.docker.io/v1/"
+                                sh "docker push ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}"
+                            }
                         }
                     }
                 }
