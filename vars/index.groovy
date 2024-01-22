8c4d6ea93c80255cdc516b288ac8032c683c6a5a
Sushant-Gandalwar <153522559+Sushant-Gandalwar@users.noreply.github.com>
2024-01-23 01:43:07 +0530

    Update index.groovy

--- vars/index.groovy
+++ vars/index.groovy
@@ -22,7 +22,7 @@ def call(Map pipelineParams) {
                 post {
                     failure {
                         script {
-                            log.error("Initialization code has an error for ${APP_Name}")
+                            error("Initialization code has an error for ${APP_Name}")
                         }
                     }
                 }
@@ -31,50 +31,43 @@ def call(Map pipelineParams) {
             stage('Build and Push Docker Image') {
                 steps {
                     script {
-                   withDockerRegistry([credentialsId: "gcr:${env.CREDENTIALS_ID}", url: "https://gcr.io"]) {
-                      sh "cd ${env.DOCKERDIRECTORY} && docker build -t '${env.IMAGE}:${env.IMAGETAG}' -f Dockerfile ."
-                     
+                        withDockerRegistry([credentialsId: "gcr:${env.CREDENTIALS_ID}", url: "https://gcr.io"]) {
+                            sh "cd ${env.DOCKERDIRECTORY} && docker build -t '${env.IMAGE}:${env.IMAGETAG}' -f Dockerfile ."
+                        }
                     }
-                    }
-                    
                 }
             }
-            stages {
-        stage('ARC-DEV APPROVAL') {
-            when {
-                expression { true } // Always evaluate to true
-            }
-            steps {
-                script {
-                    echo "Approval is required to perform deployment in DEV, Click 'Proceed or Abort'"
-                }
 
-                timeout(time: 2, unit: 'HOURS') {
-                    verifybuild()
+            stage('ARC-DEV APPROVAL') {
+                when {
+                    expression { true } // Always evaluate to true
+                }
+                steps {
+                    script {
+                        echo "Approval is required to perform deployment in DEV, Click 'Proceed or Abort'"
+                    }
+
+                    timeout(time: 2, unit: 'HOURS') {
+                        verifybuild()
+                    }
                 }
             }
-        }
+
             stage('CONTAINER') {
-                                 
                 steps {
                     script {
                         sh "docker run -p 8085:8000 ${env.IMAGE}:${env.IMAGETAG}"
-                        
                     }
-
-                    
-                }    
+                }
             }
         }
     }
 }
 
-
-
 def verifybuild() {
     def userInput = input(
         id: 'userInput', message: 'Approve Deployment!', parameters: [
-            [$class: 'BooleanParameterDefinition', defaultValue: 'false', description: 'click to skip', name: 'skip'],
+            [$class: 'BooleanParameterDefinition', defaultValue: false, description: 'click to skip', name: 'skip'],
         ])
 
     if (!userInput) {
