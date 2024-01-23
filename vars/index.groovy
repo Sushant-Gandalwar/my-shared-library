84b2d1e3a47fd86ee632161801496bf65d56ed46
Sushant-Gandalwar <153522559+Sushant-Gandalwar@users.noreply.github.com>
2024-01-23 23:13:04 +0530

    Update index.groovy

--- vars/index.groovy
+++ vars/index.groovy
@@ -6,9 +6,12 @@ def call(Map pipelineParams) {
             scmUrl = "${pipelineParams.scmUrl}"
             APP_Name = "${pipelineParams.appName}"
             DOCKERDIRECTORY = "${pipelineParams.dockerDirectory}"
-             IMAGE = "${pipelineParams.dockerImage}"
+            IMAGE = "${pipelineParams.dockerImage}"
             IMAGE_TAG = "${params.Parameter}"
             CREDENTIALS_ID = "${pipelineParams.dockerCredentialsId}"
+            GCP_PROJECT_ID = 'jenkins-407204'
+            GKE_CLUSTER_NAME = 'sushant'
+            GKE_CLUSTER_ZONE = 'us-central1'
         }
 
         stages {
@@ -42,21 +45,32 @@ def call(Map pipelineParams) {
                     }
                 }
             }
-           stage('ARC-DEV APPROVAL') {
-              
+
+            stage('Create GKE Cluster') {
+                steps {
+                    script {
+                        // Authenticate with GCP using service account credentials
+                        withCredentials([gcpServiceAccount(credentialsId:"gcr: ${env.CREDENTIALS_ID}", project: "id : ${GCP_PROJECT_ID}")]) {
+                            // Set the GCP project and zone
+                            sh "gcloud config set project ${GCP_PROJECT_ID}"
+                            sh "gcloud config set compute/zone ${GKE_CLUSTER_ZONE}"
+
+                            // Create GKE cluster
+                            sh "gcloud container clusters create ${GKE_CLUSTER_NAME} --num-nodes=3 --machine-type=n1-standard-1"
+                        }
+                    }
+                }
+            }
+
+            stage('ARC-DEV APPROVAL') {
                 steps {
                     script {
                         sh "cd ${env.DOCKERDIRECTORY}"
                         sh "kubectl apply -f demo.yaml"
                         echo "done"
                     }
-
-                   
                 }
             }
-
-            
         }
     }
 }
-
