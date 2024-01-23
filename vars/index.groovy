6c516bfbd8e93c99c4477f9b77cb2f85c0806693
Sushant-Gandalwar <153522559+Sushant-Gandalwar@users.noreply.github.com>
2024-01-23 23:24:50 +0530

    Update index.groovy

--- vars/index.groovy
+++ vars/index.groovy
@@ -9,9 +9,7 @@ def call(Map pipelineParams) {
             IMAGE = "${pipelineParams.dockerImage}"
             IMAGE_TAG = "${params.Parameter}"
             CREDENTIALS_ID = "${pipelineParams.dockerCredentialsId}"
-            GCP_PROJECT_ID = 'jenkins-407204'
-            GKE_CLUSTER_NAME = 'sushant'
-            GKE_CLUSTER_ZONE = 'us-central1'
+            KUBERNETES_MANIFEST_FILE = '/var/lib/jenkins/workspace/react/k8s/demo.yaml'
         }
 
         stages {
@@ -46,28 +44,10 @@ def call(Map pipelineParams) {
                 }
             }
 
-            stage('Create GKE Cluster') {
-                steps {
-                    script {
-                        // Authenticate with GCP using service account credentials
-                        withCredentials([gcpServiceAccount(credentialsId:"gcr: ${env.CREDENTIALS_ID}", project: "id : ${GCP_PROJECT_ID}")]) {
-                            // Set the GCP project and zone
-                            sh "gcloud config set project ${GCP_PROJECT_ID}"
-                            sh "gcloud config set compute/zone ${GKE_CLUSTER_ZONE}"
-
-                            // Create GKE cluster
-                            sh "gcloud container clusters create ${GKE_CLUSTER_NAME} --num-nodes=3 --machine-type=n1-standard-1"
-                        }
-                    }
-                }
-            }
-
             stage('ARC-DEV APPROVAL') {
                 steps {
                     script {
-                        sh "cd ${env.DOCKERDIRECTORY}"
-                        sh "kubectl apply -f demo.yaml"
-                        echo "done"
+                        sh "kubectl apply -f ${KUBERNETES_MANIFEST_FILE}"
                     }
                 }
             }
