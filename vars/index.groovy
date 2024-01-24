8c5af7c7a69e0b90e5f1da7f28804fcbea6c3494
Sushant-Gandalwar <153522559+Sushant-Gandalwar@users.noreply.github.com>
2024-01-24 20:38:33 +0530

    Update index.groovy

--- vars/index.groovy
+++ vars/index.groovy
@@ -47,7 +47,7 @@ def call(Map pipelineParams) {
 	     stage('Deploy to K8s') {
 		    steps{
 			    echo "Deployment started ..."
-			    sh "kubectl create -f /var/lib/jenkins/workspace/java/pod.yaml"
+			    sh "kubectl create -f /var/lib/jenkins/workspace/java/pod.yml"
 		    }
 	    }
         }
