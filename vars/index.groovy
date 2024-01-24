fa9b5fc78bfc3b93795f98e466ef8fe4809803d3
Sushant-Gandalwar <153522559+Sushant-Gandalwar@users.noreply.github.com>
2024-01-25 00:26:15 +0530

    Update index.groovy

--- vars/index.groovy
+++ vars/index.groovy
@@ -1,20 +1,14 @@
 def call(Map pipelineParams) {
     pipeline {
         agent any
-        tools {
-		maven 'Maven'
-	}
+
         environment {
             scmUrl = "${pipelineParams.scmUrl}"
             APP_Name = "${pipelineParams.appName}"
             DOCKERDIRECTORY = "${pipelineParams.dockerDirectory}"
-            IMAGE = "${pipelineParams.dockerImage}"
+             IMAGE = "${pipelineParams.dockerImage}"
             IMAGE_TAG = "${params.Parameter}"
             CREDENTIALS_ID = "${pipelineParams.dockerCredentialsId}"
-            PROJECT_ID = 'jenkins-407204'
-            CLUSTER_NAME = 'k8s-cluster'
-            LOCATION = 'us-central1-c'
-               
         }
 
         stages {
@@ -39,11 +33,56 @@ def call(Map pipelineParams) {
                     script {
                         withDockerRegistry([credentialsId: "gcr:${env.CREDENTIALS_ID}", url: "https://gcr.io"]) {
                             sh "cd ${env.DOCKERDIRECTORY} && docker build -t '${env.IMAGE}:${env.IMAGETAG}' -f Dockerfile ."
-                            sh "docker push '${env.IMAGE}:${env.IMAGETAG}'"
+                             sh """
+                                docker push '${env.IMAGE}:${env.IMAGETAG}'
+                               
+                                
+                                """
                         }
+                    }
+                }
+            }
+           stage('ARC-DEV APPROVAL') {
+               
+                steps {
+                    script {
+                        echo "Approval is required to perform deployment in DEV, Click 'Proceed or Abort'"
+                    }
+
+                    timeout(time: 2, unit: 'HOURS') {
+                        verifybuild()
+                    }
+                }
+            }
+
+            stage('CONTAINER') {
+                when {
+                    expression { env.releaseskip == 'dorelease' }
+                }
+                steps {
+                    script {
+                        sh "docker run -p 8083:3000 ${env.IMAGE}:${env.IMAGETAG}"
                     }
                 }
             }
         }
     }
 }
+
+def verifybuild() {
+
+        def userInput = input(
+            id: 'userInput', message: 'Approve Deployment!',        parameters: [
+
+      [$class: 'BooleanParameterDefinition', defaultValue: 'false', description: 'click to skip', name: 'skip'],
+    ])
+
+        if(!userInput) {
+            env.releaseskip = 'dorelease'
+            }
+            else {
+                env.releaseskip = 'norelease'
+
+            }
+
+    }
