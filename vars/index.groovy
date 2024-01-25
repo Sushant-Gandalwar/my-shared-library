936fa928c4c9e028bc0e6299df91182e611e3292
Sushant-Gandalwar <153522559+Sushant-Gandalwar@users.noreply.github.com>
2024-01-25 12:25:28 +0530

    Update index.groovy

--- vars/index.groovy
+++ vars/index.groovy
@@ -1,49 +1,56 @@
 pipeline {
     agent any
+
     environment {
         PROJECT_ID = 'jenkins-407204'
         CLUSTER_NAME = 'k8s-cluster'
         LOCATION = 'us-central1-c'
         CREDENTIALS_ID = 'f3d27808a72f4b4584aa7f7edd4447d1'
+        DOCKER_IMAGE_NAME = 'sushantgandalwar/hello'
     }
+
     stages {
         stage('INITIALIZE') {
-                steps {
-                    script {
-                        echo "Initializing environment for webstore delivery pipeline"
-                        echo "Git URL: ${env.scmUrl}"
-                    }
+            steps {
+                script {
+                    echo "Initializing environment for webstore delivery pipeline"
+                    echo "Git URL: ${env.scmUrl}"
                 }
-                post {
-                    failure {
-                        script {
-                            error("Initialization code has an error for ${APP_Name}")
-                        }
+            }
+            post {
+                failure {
+                    script {
+                        error("Initialization code has an error for ${APP_Name}")
                     }
                 }
             }
+        }
 
         stage("Build image") {
             steps {
                 script {
-                    myapp = docker.build("sushantgandalwar/hello:${env.BUILD_ID}")
+                    def myapp = docker.build("${env.DOCKER_IMAGE_NAME}:${env.BUILD_ID}")
                 }
             }
         }
+
         stage("Push image") {
             steps {
                 script {
                     docker.withRegistry('https://registry.hub.docker.com', 'dockerhub') {
-                            myapp.push("latest")
-                            myapp.push("${env.BUILD_ID}")
+                        docker.image("${env.DOCKER_IMAGE_NAME}:${env.BUILD_ID}").push()
+                        docker.image("${env.DOCKER_IMAGE_NAME}:latest").push()
                     }
                 }
             }
         }
+
         stage('Deploy to GKE') {
-            steps{
-                sh "sed -i 's/hello:latest/hello:${env.BUILD_ID}/g' deployment.yaml"
-                step([$class: 'KubernetesEngineBuilder', projectId: env.PROJECT_ID, clusterName: env.CLUSTER_NAME, location: env.LOCATION, manifestPattern: 'deployment.yaml', credentialsId: env.CREDENTIALS_ID, verifyDeployments: true])
+            steps {
+                script {
+                    sh "sed -i 's|${env.DOCKER_IMAGE_NAME}:latest|${env.DOCKER_IMAGE_NAME}:${env.BUILD_ID}|g' deployment.yaml"
+                    step([$class: 'KubernetesEngineBuilder', projectId: env.PROJECT_ID, clusterName: env.CLUSTER_NAME, location: env.LOCATION, manifestPattern: 'deployment.yaml', credentialsId: env.CREDENTIALS_ID, verifyDeployments: true])
+                }
             }
         }
     }
