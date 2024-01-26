02e2516eb75362fb6c15a16905cfac7d3cfd9d97
Sushant-Gandalwar <153522559+Sushant-Gandalwar@users.noreply.github.com>
2024-01-26 13:21:06 +0530

    Update index.groovy

--- vars/index.groovy
+++ vars/index.groovy
@@ -7,7 +7,7 @@ def call(Map pipelineParams) {
             APP_Name = "${pipelineParams.appName}"
             DOCKERDIRECTORY = "${pipelineParams.dockerDirectory}"
             IMAGE = "${pipelineParams.dockerImage}"
-            IMAGE_TAG = 'latest'
+            IMAGE_TAG = "${params.Parameter}"
             NEW_IMAGE_NAME = "sushant"  // Specify the new name for the image
             CREDENTIALS_ID = "${pipelineParams.dockerCredentialsId}"
             PROJECT_ID = 'jenkins-407204'
@@ -32,16 +32,16 @@ def call(Map pipelineParams) {
                 }
             }
 
-            stage('Build and Push Docker Image') {
+            stage('Build, Rename, and Push Docker Image') {
                 steps {
                     script {
                         withDockerRegistry([credentialsId: "gcr:${env.CREDENTIALS_ID}", url: "https://gcr.io"]) {
                             sh "cd ${env.DOCKERDIRECTORY} && docker build -t '${env.IMAGE}:${env.IMAGE_TAG}' -f Dockerfile ."
+                            sh "docker tag '${env.IMAGE}:${env.IMAGE_TAG}' '${env.IMAGE}:${env.NEW_IMAGE_NAME}'"
 
                             sh """
                                 docker push '${env.IMAGE}:${env.IMAGE_TAG}'
-                                docker tag '${env.IMAGE}:${env.IMAGE_TAG}' '${env.NEW_IMAGE_NAME}:${env.IMAGE_TAG}'
-                                docker push '${env.NEW_IMAGE_NAME}:${env.IMAGE_TAG}'
+                                docker push '${env.IMAGE}:${env.NEW_IMAGE_NAME}'
                             """
                         }
                     }
