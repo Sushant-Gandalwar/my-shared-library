cf21f0eb955327ab718a11dc0ed4430eeb82ea80
Sushant-Gandalwar <153522559+Sushant-Gandalwar@users.noreply.github.com>
2024-01-27 00:49:04 +0530

    Update index.groovy

--- vars/index.groovy
+++ vars/index.groovy
@@ -1,31 +1,12 @@
 def call(Map pipelineParams) {
     pipeline {
         agent any   
-        parameters {
-            choice(name: 'Build_Type', choices: "BUILD&DEPLOY&Publish_to_snapshot\nDEPLOY_ONLY\nPublish_to_Release", description: 'Select the Build type')
-            string(name: 'Parameter', defaultValue: 'default', description: 'Pass the Docker image id if chosen DEPLOY_ONLY OR pass the sbt release command if chosen Publish_to_Release')
-        }        
-
-        environment {
-            APP_NAME = "${pipelineParams.appName}"
-            DOCKERDIRECTORY = "${pipelineParams.dockerDirectory}"
-            IMAGE = "${pipelineParams.dockerImage}"
-            CREDENTIALS_ID = "${pipelineParams.dockerCredentialsId}"
-            DB_CREDS_ID = "${pipelineParams.databaseCredentialsId}"
-            IMAGE_TAG = "${params.Parameter}"
-            COMMAND = "${params.Parameter}"
-            BRANCH = "${pipelineParams.branch}"
-            EMAIL = "cicd-admin@aienterprise.com"
-            USERNAME = "cicd-admin-aie"
-        }
-
         stages {
             stage('INITIALIZE') {
                 steps {
                     script {
                         echo 'Start Initializing!'
-                        cleanWs()  // Clean the workspace
-                        checkout scmUrl  // Checkout the repository
+                       
                     }
                 }
             }
