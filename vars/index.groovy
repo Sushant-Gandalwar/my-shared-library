ac74c66bfe1b5b25b837bee43ee8177890ecfa04
Sushant-Gandalwar <153522559+Sushant-Gandalwar@users.noreply.github.com>
2024-01-27 00:56:40 +0530

    Update index.groovy

--- vars/index.groovy
+++ vars/index.groovy
@@ -1,15 +1,38 @@
 def call(Map pipelineParams) {
     pipeline {
-        agent any   
+        agent any
+
+        environment {
+            scmUrl = "${pipelineParams.scmUrl}"
+            APP_Name = "${pipelineParams.appName}"
+            DOCKERDIRECTORY = "${pipelineParams.dockerDirectory}"
+            IMAGE = "${pipelineParams.dockerImage}"
+            IMAGE_TAG = 'demo'
+            NEW_IMAGE_NAME = "react"  // Specify the new name for the image
+            CREDENTIALS_ID = "${pipelineParams.dockerCredentialsId}"
+            PROJECT_ID = 'jenkins-407204'
+            CLUSTER_NAME = 'demo'
+            LOCATION =  'us-central1'
+        }
+
         stages {
             stage('INITIALIZE') {
                 steps {
                     script {
-                        echo 'Start Initializing!'
-                       
+                        echo "Initializing environment for webstore delivery pipeline"
+                        echo "Git URL: ${env.scmUrl}"
+                        echo "env.BUILD_NUMBER"
+                    }
+                }
+                post {
+                    failure {
+                        script {
+                            error("Initialization code has an error for ${APP_Name}")
+                        }
                     }
                 }
             }
+
         }
     }
 }
