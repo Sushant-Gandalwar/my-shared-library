b5a22808aeb4378fa0c2d68daecb781c243810c9
Sushant-Gandalwar <153522559+Sushant-Gandalwar@users.noreply.github.com>
2024-01-27 01:01:23 +0530

    Update index.groovy

--- vars/index.groovy
+++ vars/index.groovy
@@ -32,6 +32,35 @@ def call(Map pipelineParams) {
                 }
             }
 
+            stage('Build, Rename, and Push Docker Image') {
+                steps {
+                    script {
+                        withDockerRegistry([credentialsId: "gcr:${env.CREDENTIALS_ID}", url: "https://gcr.io"]) {
+                            sh "cd ${env.DOCKERDIRECTORY} && docker build -t '${env.IMAGE}:${env.IMAGE_TAG}' -f Dockerfile ."
+                            sh "docker tag '${env.IMAGE}:${env.IMAGE_TAG}' '${env.IMAGE}:${env.NEW_IMAGE_NAME}'"
+
+                            sh """
+                                docker push '${env.IMAGE}:${env.IMAGE_TAG}'
+                                docker push '${env.IMAGE}:${env.NEW_IMAGE_NAME}'
+                            """
+                        }
+                    }
+                }
+            }
+            stage('Deploy to K8s') {
+		    steps{
+			   echo "Deployment started ..."
+			    sh 'ls -ltr'
+			    sh 'pwd'
+			    sh "sed -i 's/tagversion/${env.BUILD_ID}/g' serviceLB.yaml"
+				sh "sed -i 's/tagversion/${env.BUILD_ID}/g' deployment.yaml"
+			    echo "Start deployment of serviceLB.yaml"
+			    step([$class: 'KubernetesEngineBuilder', projectId: env.PROJECT_ID, clusterName: env.CLUSTER_NAME, location: env.LOCATION, manifestPattern: 'serviceLB.yaml', credentialsId: env.CREDENTIALS_ID, verifyDeployments: true])
+				echo "Start deployment of deployment.yaml"
+				step([$class: 'KubernetesEngineBuilder', projectId: env.PROJECT_ID, clusterName: env.CLUSTER_NAME, location: env.LOCATION, manifestPattern: 'deployment.yaml', credentialsId: env.CREDENTIALS_ID, verifyDeployments: true])
+			    echo "Deployment Finished ..."
+		    }
+	    }
         }
     }
 }
