931d9ce6bc59811092b7ab575648f3d59242a160
Sushant-Gandalwar <153522559+Sushant-Gandalwar@users.noreply.github.com>
2024-01-26 23:59:53 +0530

    Update index.groovy

--- vars/index.groovy
+++ vars/index.groovy
@@ -2,21 +2,15 @@ def call(Map pipelineParams) {
     pipeline {
         agent any
 
-        parameters {
-            string(name: 'Parameter', defaultValue: 'default', description: 'Pass the Docker image id if choosed DEPLOY_ONLY OR pass the sbt release command if choosed Publish_to_Release')
-        }
+       parameters {
+          choice(name: 'Build_Type', choices: "BUILD&DEPLOY&Publish_to_snapshot\nDEPLOY_ONLY\nPublish_to_Release", description: 'Select the Build type' )
+           string(name: 'Parameter', defaultValue: 'default', description: 'Pass the Docker image id if choosed DEPLOY_ONLY OR pass the sbt release command if choosed Publish_to_Release', )
+        }  
 
         environment {
             APP_Name = "${pipelineParams.appName}"
-            DOCKERDIRECTORY = "${pipelineParams.dockerDirectory}"
             IMAGE = "${pipelineParams.dockerImage}"
             IMAGE_TAG = "${params.Parameter}"
-            COMMAND = "${params.Parameter}"
-            CREDENTIALS_ID = "${pipelineParams.dockerCredentialsId}"
-            PROJECT_ID = 'jenkins-407204'
-            CLUSTER_NAME = 'demo'
-            LOCATION =  'us-central1'
-            BRANCH =  "${pipelineParams.branch}"
         }
 
         stages {
