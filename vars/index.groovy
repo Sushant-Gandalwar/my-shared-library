7849b5c4204de488612858d5e306d15646935d26
Sushant-Gandalwar <153522559+Sushant-Gandalwar@users.noreply.github.com>
2024-01-27 09:20:01 +0530

    Update index.groovy

--- vars/index.groovy
+++ vars/index.groovy
@@ -7,12 +7,11 @@ def call(Map pipelineParams) {
             APP_Name = "${pipelineParams.appName}"
             DOCKERDIRECTORY = "${pipelineParams.dockerDirectory}"
             IMAGE = "${pipelineParams.dockerImage}"
-            IMAGE_TAG = 'demo'
-            NEW_IMAGE_NAME = "react"  // Specify the new name for the image
             CREDENTIALS_ID = "${pipelineParams.dockerCredentialsId}"
             PROJECT_ID = 'jenkins-407204'
             CLUSTER_NAME = 'demo'
             LOCATION =  'us-central1'
+            IMAGE_TAG = "${env.BUILD_NUMBER}"  // Set image tag to the build number
         }
 
         stages {
@@ -21,13 +20,23 @@ def call(Map pipelineParams) {
                     script {
                         echo "Initializing environment for webstore delivery pipeline"
                         echo "Git URL: ${env.scmUrl}"
-			echo "Current Build Number: ${env.BUILD_NUMBER}"
                     }
                 }
                 post {
                     failure {
                         script {
                             error("Initialization code has an error for ${APP_Name}")
+                        }
+                    }
+                }
+            }
+
+            stage('Build, Rename, and Push Docker Image') {
+                steps {
+                    script {
+                        withDockerRegistry([credentialsId: "gcr:${env.CREDENTIALS_ID}", url: "https://gcr.io"]) {
+                            sh "cd ${env.DOCKERDIRECTORY} && docker build -t '${env.IMAGE}:${env.IMAGE_TAG}' -f Dockerfile ."
+                            sh "docker push '${env.IMAGE}:${env.IMAGE_TAG}'"
                         }
                     }
                 }
