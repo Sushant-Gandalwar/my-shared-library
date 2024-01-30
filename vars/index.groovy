866bb2367b55940dba11de63aaf15a2be7e0e64e
Sushant-Gandalwar <153522559+Sushant-Gandalwar@users.noreply.github.com>
2024-01-30 18:04:36 +0530

    Update index.groovy

--- vars/index.groovy
+++ vars/index.groovy
@@ -1,13 +1,24 @@
 def call(Map pipelineParams) {
     pipeline {
         agent any
+        environment {
+            scmUrl = "${pipelineParams.scmUrl}"
+            APP_Name = "${pipelineParams.appName}"
+            DOCKERDIRECTORY = "${pipelineParams.dockerDirectory}"
+            IMAGE = "${pipelineParams.dockerImage}"
+            CREDENTIALS_ID = "${pipelineParams.dockerCredentialsId}"
+            PROJECT_ID = 'jenkins-407204'
+            CLUSTER_NAME = 'demo'
+            LOCATION =  'us-central1'
+            IMAGE_TAG = "${env.BUILD_NUMBER}"  
+        }
 
         stages {
             stage('INITIALIZE') {
                 steps {
                     script {
                         echo "Initializing environment for webstore delivery pipeline"
-                        git branch: 'main', credentialsId: 'sushant-git', url: 'https://github.com/Sushant-Gandalwar/Jenkins-Docker-Kubernetes-Project3.git'
+                        git branch: 'main', credentialsId: 'sushant-git', url: pipelineParams.scmUrl
                     }
                 }
                 post {
