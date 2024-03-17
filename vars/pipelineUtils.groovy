918bb210159114eae876d8e07481391c8e3d41d4
Sushant-Gandalwar <153522559+Sushant-Gandalwar@users.noreply.github.com>
2024-03-17 20:20:30 +0530

    Update pipelineUtils.groovy

--- vars/pipelineUtils.groovy
+++ vars/pipelineUtils.groovy
@@ -62,14 +62,17 @@ def call(Map pipelineParams) {
                     echo "Deployment started ..."
                     sh 'ls -ltr'
                     sh 'pwd'
-                    sh "sed -i 's/tagversion/${env.BUILD_NUMBER}/g' serviceLB.yaml"
+                   
                     sh "sed -i 's/tagversion/${env.BUILD_NUMBER}/g' deployment.yaml"
                     
-                    echo "Start deployment of serviceLB.yaml"
-                    step([$class: 'KubernetesEngineBuilder', projectId: env.PROJECT_ID, clusterName: env.CLUSTER_NAME, location: env.LOCATION, manifestPattern: 'serviceLB.yaml', credentialsId: env.CREDENTIALS_ID, verifyDeployments: true])
-                    
+                  
                     echo "Start deployment of deployment.yaml"
                     step([$class: 'KubernetesEngineBuilder', projectId: env.PROJECT_ID, clusterName: env.CLUSTER_NAME, location: env.LOCATION, manifestPattern: 'deployment.yaml', credentialsId: env.CREDENTIALS_ID, verifyDeployments: true])
+
+                     sh "sed -i 's/tagversion/${env.BUILD_NUMBER}/g' serviceLB.yaml"
+
+                    echo "Start deployment of serviceLB.yaml"
+                    step([$class: 'KubernetesEngineBuilder', projectId: env.PROJECT_ID, clusterName: env.CLUSTER_NAME, location: env.LOCATION, manifestPattern: 'serviceLB.yaml', credentialsId: env.CREDENTIALS_ID, verifyDeployments: true])
                     
                     echo "Deployment Finished ..."
                 }
